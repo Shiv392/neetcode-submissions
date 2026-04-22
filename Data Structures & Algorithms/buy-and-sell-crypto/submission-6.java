db8f2a96593556120eca82294c1228ca2485808a
Shiv Soni <109019273+Shiv392@users.noreply.github.com>
2026-04-22 15:09:50 +0530

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int length=prices.length;
+        int []leftmin_arr = new int[length];
+        int []rightmax_arr = new int[length];
+        leftmin_arr[0]=prices[0];
+        rightmax_arr[length-1]=prices[length-1];
+
+        //get the min price from the left side
+        for(int i=1;i<length;i++){
+            leftmin_arr[i] = Math.min(prices[i], leftmin_arr[i-1]);
+        }
+        //get the max price from the right side
+        for(int i=length-2;i>=0;i--){
+            rightmax_arr[i] = Math.max(prices[i], rightmax_arr[i+1]);
+        }
+
+        int max_profit=0;
+        for(int i=0;i<length;i++){
+            max_profit = Math.max(max_profit, Math.abs(leftmin_arr[i]-rightmax_arr[i]));
+        }
+        return max_profit;
+    }
+}
